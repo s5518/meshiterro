8087996c7bb33beab4317c087f5ae0de0a4092f2
s5518 <306053809+s5518@>
2026-08-14 03:38:51 +0000

    [Finish] 4投稿機能の作成(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  has_many :post_images, dependent: :destroy
+  
 end
