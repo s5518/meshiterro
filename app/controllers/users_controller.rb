caec0480c9296f5160f0651d975f428b633f1bdf
s5518 <306053809+s5518@>
2026-08-14 12:15:45 +0000

    [Finish] 4ユーザーに関する機能(3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,20 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
  
   private
  
   def user_params
-    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation, :profile_image)
   end
 end
